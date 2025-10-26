31cb3a0fd410f28610b6e6562a3c1a2d0dad50a0
Kamal Hosny <ixonhosny@gmail.com>
2025-10-26 04:53:32 +0300

    countSmileys

--- solution.js
+++ solution.js
@@ -1,12 +1,6 @@
-var number = function(busStops){
-  // Good Luck!
-  let people = 0 
-    for (let i = 0; i < busStops.length; i++) {
-        people += busStops[i][0]
-        people -= busStops[i][1]
-        
-    }
-  return people
-}
+function countSmileys(arr) {
 
-console.log(number([[3, 0], [5, 2], [2, 1]]));
+  const regex = /^[:;][-~]?[)D]$/;
+  
+  return arr.filter(face => regex.test(face)).length;
+}
